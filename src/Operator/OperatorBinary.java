9041af98c6e277d90f62df48fbb2bc58b272f8d2
kamilah82 <kamilah.uniska@gmail.com>
2020-05-02 09:31:04 +0700

    Operator Binary Assignment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Mirza Yogy Kurniawan";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+}
